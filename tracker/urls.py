3c4ccafecfbd6e53fbe747f6864db6570f582ee6
dougfoo <doug.cha@gmail.com>
2019-07-14 18:28:16 -0400

    minor cleanup views

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:homicide_id>', views.detail, name='detail'),
+#    path('<int:homicide_id>', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:homicide_id>/article', views.article, name='article'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from .models import Article, Homicide
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
+from django.views import generic
 
 def index(request):
     h_list = Homicide.objects.order_by('-date')[:5]
@@ -10,10 +11,10 @@ def index(request):
     return HttpResponse(template.render(context, request))
     # return render(request, 'tracker/index.html', context)
 
-def detail(request, homicide_id):
-    homicide = get_object_or_404(Homicide, pk=homicide_id)
-    return render(request, 'tracker/detail.html', {'homicide': homicide})
+class DetailView(generic.DetailView):
+    model = Homicide
+    template_name = 'tracker/detail.html'
 
 def article(request, homicide_id):
-    return HttpResponse("You're looking at articles for homicide %s." % homicide_id)
-
+    article = get_object_or_404(Article, pk=homicide_id)
+    return render(request, 'tracker/article.html', {'article': article})
