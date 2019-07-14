432dfebfd73215ca67d26ae7b783005b4e2280c0
dougfoo <doug.cha@gmail.com>
2019-07-14 16:42:59 -0400

    one model one view start

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Homicide(models.Model):
+    date = models.DateField()
+    street = models.CharField(max_length=80)
+    intersection = models.CharField(max_length=120)
+    zipcode = models.CharField(max_length=5)
+    gender = models.CharField(max_length=1)
+    age = models.IntegerField()
+
+class Article(models.Model):
+    headline = models.CharField(max_length=250)
+    homicide = models.ManyToManyField(Homicide)
+
+
