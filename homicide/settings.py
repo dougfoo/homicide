358d45e75e08d84415cb66c195505c6bedbfc6b8
dougfoo <doug.cha@gmail.com>
2019-07-14 23:36:48 -0400

    added requirements and opened to *

--- homicide/settings.py
+++ homicide/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_qq_plf-nu*mmwms!vw-r2o2xd)r$$4&(y%$$vhb&*plhs$q+q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
