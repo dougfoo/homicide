0d6582ed199e937cef648fd4118d6212fd12c5af
dougfoo <doug.cha@gmail.com>
2019-07-21 17:20:02 -0400

    ssl off again...

--- homicide/settings.py
+++ homicide/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '_qq_plf-nu*mmwms!vw-r2o2xd)r$$4&(y%$$vhb&*plhs$q+q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 
 ALLOWED_HOSTS = ['*']
 
