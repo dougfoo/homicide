56a35869c0591ba445d242b7ecf134c0274ab948
dougfoo <doug.cha@gmail.com>
2019-07-29 22:07:08 -0400

    rolling back Window func,could be some version issue w/ sqlite driver or py 3.7.3 vs 3.7

--- tracker/views.py
+++ tracker/views.py
@@ -13,14 +13,14 @@ import numpy as np
 class HomicideListView(generic.ListView):
     model = Homicide
     template_name = 'tracker/detail.html'
-    #queryset = Homicide.objects.all().order_by('-date')
-    queryset = Homicide.objects.all().order_by('-date').annotate(r=Window(expression=RowNumber(),order_by=[F('date')]))
+    queryset = Homicide.objects.all().order_by('-date')
+    #queryset = Homicide.objects.all().order_by('-date').annotate(r=Window(expression=RowNumber(),order_by=[F('date')]))
     context_object_name = 'h_list'
 
 def detail(request, homicide_id):
     homicide = get_object_or_404(Homicide, pk=homicide_id)
-    #h_list = Homicide.objects.all().order_by('-date')
-    h_list = Homicide.objects.all().order_by('-date').annotate(r=Window(expression=RowNumber(),order_by=[F('date')]))
+    h_list = Homicide.objects.all().order_by('-date')
+    #h_list = Homicide.objects.all().order_by('-date').annotate(r=Window(expression=RowNumber(),order_by=[F('date')]))
     return render(request, 'tracker/detail.html', {'h_list': h_list, 'homicide': homicide })
 
 class ArticleListView(generic.ListView):
