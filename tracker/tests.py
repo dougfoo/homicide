b817c28ce1c311cc99c3bdd3fb8d5e1390b8584f
dougfoo <doug.cha@gmail.com>
2019-08-02 21:36:57 -0400

    test added

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from tracker.models import Homicide, Article 
 
-# Create your tests here.
+# models test
+class HomicideTest(TestCase):
+
+    def create_article(self, url='http://foo.com', headline='this is the headline', content='this is the content'):
+        return Article.objects.create(url=url, headline=headline, content=content)
+
+    def test_article_creation(self):
+        w = self.create_article()
+        self.assertTrue(isinstance(w, Article))
+
+    def test_homicide_creation(self):
+        w = self.create_article()
+        self.assertTrue(isinstance(w, Article))
