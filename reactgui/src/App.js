8c46cfaf80bd98727a559cd483374eefc9ad35d0
dougfoo <doug.cha@gmail.com>
2019-08-19 23:54:49 -0400

    ract initial table view

--- reactgui/src/App.js
+++ reactgui/src/App.js
@@ -1,24 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Table, message } from 'antd';
+import axios from 'axios';
 import './App.css';
+
+class Foo extends React.Component {
+  state = { 
+    visible: false,
+    homicides: [],
+  }
+
+  componentDidMount() {
+    console.log('consolemount');
+    try {
+      axios.get('http://localhost:8000/homicides/').then(response => response.data)
+          .then((data) => {
+            console.log('homicides',data);
+            this.setState({ homicides: data });
+          })
+          .catch(function (error) {
+              message.error("Axios backend loans error: "+error);
+          })
+    } catch (error) {
+      console.error(error);
+      message.error("Axios unhandled error: "+error);
+    } 
+  }
+
+  render() {
+    const data = this.state.homicides;
+    const columns = [
+      {
+        title: 'Date',
+        dataIndex: 'date',
+        key: 'date',
+        render: text => <a href="/homicides/">{text}</a>,
+      },
+      { title: 'Name', dataIndex: 'name', key: 'name', },
+      { title: 'Age', dataIndex: 'age', key: 'age', },
+      { title: 'Gender', dataIndex: 'gender', key: 'gender', },
+      { title: 'Ethnic', dataIndex: 'ethnicity', key: 'ethnicity', },
+      { title: 'Means', dataIndex: 'means', key: 'means', },
+      { title: 'Street', dataIndex: 'street', key: 'street', },
+    ];
+
+    return (
+      <div>
+        <span>
+          Hello World
+        </span>
+        <Table columns={columns} expandedRowRender={record => <p style={{ margin: 0 }}>{record.description}</p>} dataSource={data} />,
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React And DASH
-        </a>
-      </header>
+      <Foo/>
     </div>
   );
 }
